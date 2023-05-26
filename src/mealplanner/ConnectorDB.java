560ab7a3d35c4601ce9b867004e1fb401cdf3022
Denis Stoyanouski <vandenst@tut.by>
2023-05-26 16:28:58 +0300

    add method addNewMeal and getMealId to ConnectorDB class

--- src/mealplanner/ConnectorDB.java
+++ src/mealplanner/ConnectorDB.java
@@ -1,9 +1,7 @@
 package mealplanner;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.List;
 
 public class ConnectorDB {
 
@@ -35,17 +33,65 @@ public class ConnectorDB {
             statement.executeUpdate("create table meals (" +
                     "category varchar," +
                     "meal varchar," +
-                    "meal_id integer NOT NULL" +
+                    "meal_id integer SERIAL" +
                     ")");
             statement.executeUpdate("drop table if exists ingredients");
             statement.executeUpdate("create table ingredients (" +
                     "ingredient varchar," +
-                    "ingredient_id integer NOT NULL," +
+                    "ingredient_id integer SERIAL," +
                     "meal_id integer NOT NULL" +
                     ")");
             System.out.println("Tables are created");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void addNewMeal(String category, String name) {
+        String insert = "INSERT INTO meals (category, name) VALUES (?, ?)";
+        try (PreparedStatement preparedStatement = connection.prepareStatement(insert)) {
+            preparedStatement.setString(1, category);
+            preparedStatement.setString(2, name);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int getMealID(String meal) {
+        String getter = "SELECT meal_id FROM meals WHERE meal = ?";
+        int mealId = 0;
+        try (PreparedStatement preparedStatement = connection.prepareStatement(getter)) {
+            preparedStatement.setString(1, meal);
+            try (ResultSet categoryRows = preparedStatement.executeQuery()) {
+                while (categoryRows.next()) {
+                    // Retrieve column values
+                    mealId = categoryRows.getInt("meal_id");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return mealId;
+    }
+
+    public void addIngredientsForMeal(String name, List<String> ingredients) {
+        int meal_id = getMealID(name);
+        String insert = "INSERT INTO ingredients (ingredient, meal_id) VALUES (?, ?)";
+        for (String ingredient : ingredients) {
+            try (PreparedStatement preparedStatement = connection.prepareStatement(insert)) {
+                preparedStatement.setString(1, ingredient);
+                preparedStatement.setInt(2, meal_id);
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void showAllMeals() {
+        
     }
 }
